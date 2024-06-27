34c42a5a25d1dbebabf76ea170939a7ce651d9a4
David Dawson <david.dawson@gmail.com>
2024-06-27 15:33:07 +0100

    wip

--- main.tf
+++ main.tf
@@ -192,9 +192,13 @@ resource "aws_security_group_rule" "egress" {
 }
 
 module "dns_host_name" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
-  enabled = length(var.dns_zone_id) > 0 && var.enabled ? true : false
-  name    = var.host_name
-  zone_id = var.dns_zone_id
-  records = coalescelist(aws_db_instance.default.*.address, [""])
+  source  = "cloudposse/route53-cluster-hostname/aws"
+  version = "0.12.2"
+
+  enabled  = length(var.dns_zone_id) > 0 && module.this.enabled
+  dns_name = var.host_name
+  zone_id  = var.dns_zone_id
+  records  = coalescelist(aws_db_instance.default[*].address, [""])
+
+  context = module.this.context
 }
